db5869530c3a6aecd205a27da0d7ecbef2484cf6
yedols <yewon0614831@gmail.com>
2025-07-08 01:41:02 +0900

    ✨ PWA 전환 및 CORS 설정, API 환경변수 적용 완료

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # 프론트 도메인
+    allow_origins=["*"],  # 프론트 도메인 -> 테스트용 이기 때문에 나중에 서비스 배포할 때는 *로 사용 X
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
