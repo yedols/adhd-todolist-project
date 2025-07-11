0f60bc50313bfcd71e2986cc05f9bb9cbd5944da
yedols <yewon0614831@gmail.com>
2025-07-11 13:15:06 +0900

    db내용 수정

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite+aiosqlite:///./todos.db"
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
